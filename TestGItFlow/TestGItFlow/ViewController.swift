52d95b998a50a5d62ec86273768699bf9e2858f7
BabyKuo <1970226903@qq.com>
2018-01-13 18:32:47 +0800

    init add gitignore
    Signed-off-by: BabyKuo <1970226903@qq.com>

--- TestGItFlow/TestGItFlow/ViewController.swift
+++ TestGItFlow/TestGItFlow/ViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let SCREENWIDTH = UIScreen.main.bounds.width
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+
+    lazy var table: UITableView = {
+        let _table = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width , height: UIScreen.main.bounds.width))
+        
+        _table.delegate = self
+        _table.dataSource = self
+        
+        return _table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "indexPath = \(indexPath.row)"
+        return cell
     }
 
 
