7ddefabe348b35ec51c9d961be3667fae8f59088
ZouFoo <1970226903@qq.com>
2018-09-27 10:58:28 +0800

    pods

---
+++ FooAnimation-Master/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FooAnimation-Master
+//
+//  Created by ZouDafu on 2018/9/27.
+//  Copyright © 2018年 ZouFoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'FooAnimation-Master' do
+  pod 'SnapKit'
+  
+end
+
+
