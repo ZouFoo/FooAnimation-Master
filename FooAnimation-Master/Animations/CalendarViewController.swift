b3998e7294221a687902ccfeac4e59ed72786189
ZouFoo <1970226903@qq.com>
2018-10-15 14:13:07 +0800

    calendar events

--- FooAnimation-Master/Animations/CalendarViewController.swift
+++ FooAnimation-Master/Animations/CalendarViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import JTAppleCalendar
 import FSCalendar
-
+import EventKit
 
 class CalendarViewController: UIViewController {
 
+    
+    // MARK: - Attributes
     let outsideMonthColor = UIColor(colorWithHexValue: 0x584a66)
     let monthColor = UIColor.white
     let selectedMonthColor = UIColor(colorWithHexValue: 0x3a294b)
@@ -20,8 +22,11 @@ class CalendarViewController: UIViewController {
 
     let formatter = DateFormatter()
     
+    var gregorianCalendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
+
     
     
+    //MARK: - UI
     lazy var yearLabel: UILabel = {
         let _label = UILabel(frame: CGRect(x: 10, y: 108, width: 100, height: 20))
         _label.font = UIFont.systemFont(ofSize: 16)
@@ -54,9 +59,21 @@ class CalendarViewController: UIViewController {
         calendarView.minimumInteritemSpacing = 0
         
         
-        
         return calendarView
     }()
+    
+    
+    let rightButton : UIButton = {
+        let rightButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        rightButton.setTitle("```", for: .normal)
+//        rightButton.addTarget(self, action: "showMoreAction", for: .touchUpInside)
+        rightButton.addTarget(self, action: #selector(showMoreAction), for: UIControl.Event.touchUpInside)
+        
+        return rightButton
+    }()
+    
+    
+    // MARK: - life
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,6 +92,77 @@ class CalendarViewController: UIViewController {
         view.addSubview(monthLabel)
         view.backgroundColor = UIColor(colorWithHexValue: 0x554467)
         
+        setup()
+    }
+    
+    func setup() {
+        
+        let rightbtn = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        rightbtn.setTitle("```", for: .normal)
+        rightbtn.setTitleColor(.blue, for: .normal)
+        rightbtn.addTarget(self, action: #selector(showMoreAction), for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightbtn)
+    
+        let store = EKEventStore()
+        let eArray = store.calendars(for: EKEntityType.event)
+        print(eArray)
+    }
+    
+    @objc func showMoreAction() {
+        print("tap right barbutton")
+        
+        let store = EKEventStore()
+        store.requestAccess(to: .event) { (bool, error) in
+            if bool == false {
+                //  没必要的三目运算
+                let alert = UIAlertController(title: bool ?"⏰ 初始化成功" : "⏰ 初始化失败", message: nil, preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+            
+        }
+        let newEvent = EKEvent(eventStore: store)
+        let alarm = EKAlarm(relativeOffset: -60 * 15) //（提醒时间） 以开始时间为0点，负前正后
+        newEvent.title =  "这是我的事件2" //标题
+        newEvent.notes = "吃完饭 来一根去不" //备注
+        newEvent.addAlarm(alarm) // 添加提醒
+        newEvent.startDate = Date() // 事件开始时间
+        newEvent.endDate = newEvent.startDate.addingTimeInterval(300) // 事件结束时间
+        newEvent.calendar = store.defaultCalendarForNewEvents
+
+        do {
+            try store.save(newEvent, span: .thisEvent)
+            let alert = UIAlertController(title: "⏰ 提醒添加成功", message: nil, preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+            
+        } catch{ //} let error as NSError {
+            let alert = UIAlertController(title:"⏰ 提醒添加失败", message: nil, preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        
+        //删除事件
+//        let store = EKEventStore()
+//        store.requestAccess(to: .event) { (bool, error) in
+//            print(bool ? "⏰ 初始化成功" : "⏰ 初始化失败")
+//        }
+//        do {
+//            try store.remove(yourevent, span: .thisEvent)
+//            print("⏰ 提醒删除成功")
+//        } catch let error as NSError {
+//            print (error, "⏰ 提醒删除失败")
+//        }
+
+
     }
     
     func handleCellTextColor(view: JTAppleCell?, cellState: CellState) {
@@ -186,12 +274,50 @@ extension CalendarViewController: FSCalendarDelegate, FSCalendarDataSource {
         fsCalendar.delegate = self
         fsCalendar.dataSource = self
         fsCalendar.appearance.headerMinimumDissolvedAlpha = 0;
+        fsCalendar.scope = .week
+        fsCalendar.appearance.weekdayTextColor = .orange
+        fsCalendar.appearance.titleDefaultColor = .white
+        fsCalendar.appearance.headerTitleColor = .white
+
+
+        fsCalendar.headerHeight = 0
         
         self.view.addSubview(fsCalendar)
         
         
+    }
+    
+
+    //今天显示的文字
+    func calendar(_ calendar: FSCalendar, titleFor date: Date) -> String? {
+        if (self.gregorianCalendar?.isDateInToday(date))!{
+            return "今天"
+        }
+        return nil
+    }
+    
+    //显示圆点事件
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        let string1 = "2018-10-18"
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "YYYY-MM-dd"
+        let newDate = dateformatter.date(from: string1)
         
+        print(date)
+        print(newDate!)
+        if date == newDate{
+            return  2
+        }
+        
+        return 1
+    }
+    
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        print(calendar.currentPage)
         
     }
     
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
 }
